120b03ff44274d6670fece9335b58de56b3258f6
Ivan <devskyfly@gmail.com>
2019-01-16 15:09:49 +0300

    Add methods to console/auth/user.

--- console/auth/UserController.php
+++ console/auth/UserController.php
@@ -2,11 +2,13 @@
 namespace devskyfly\yiiModuleAdminPanel\console\auth;
 
 use devskyfly\php56\core\Cls;
+use devskyfly\php56\types\Vrbl;
 use devskyfly\yiiModuleAdminPanel\models\auth\User;
 use Yii;
 use yii\console\Controller;
 use yii\helpers\BaseConsole;
 use yii\web\IdentityInterface;
+use yii\console\ExitCode;
 
 class UserController extends Controller
 {
@@ -31,6 +33,11 @@ class UserController extends Controller
         }
     }
     
+    /**
+     * Show list of all users
+     * 
+     * @return number
+     */
     public function actionIndex()
     {
         try {
@@ -41,22 +48,214 @@ class UserController extends Controller
             if(!empty($users)){
                 foreach ($users as $item){
                     $itr++;
-                    BaseConsole::output("{$itr}. {$item['username']}  ['{$item['email']}']");
+                    $status=$item['status']==User::STATUS_ACTIVE?'Y':'N';
+                    BaseConsole::output("{$itr}. {$status} {$item['username']}  ['{$item['email']}'] "
+                    .PHP_EOL."password_hash: {$item['password_hash']}"
+                    .PHP_EOL."auth_key: {$item['auth_key']}");
                 }
             }else{
                 BaseConsole::output('User list is empty.');
             }
         }catch (\Exception $e){
             BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
-            return -1;
+            return ExitCode::UNSPECIFIED_ERROR;
         }
         catch (\Throwable $e){
             BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
-            return -1;
+            return ExitCode::UNSPECIFIED_ERROR;
         }
         
-        return 0;
+        return ExitCode::OK;
     }
+    
+    /**
+     * Set password for user
+     */
+    public function actionSetPassword()
+    {
+        try{
+            $user_cls=static::$user_cls;
+            $app=Yii::$app;
+            
+            $user_name=BaseConsole::prompt('Enter user name:');
+            
+            $user=User::findOne(["username"=>$user_name]);
+            
+            if(Vrbl::isNull($user)){
+                BaseConsole::stdout("There is no such user '{$user_name}'.".PHP_EOL);
+                return ExitCode::UNSPECIFIED_ERROR;
+            }
+            
+            $password_1 = BaseConsole::input("Insert password:");
+            $password_2 = BaseConsole::input("Password again:");
+            
+            if($password_1!==$password_2){
+                BaseConsole::stdout("Passwords are not equal.");
+                return ExitCode::UNSPECIFIED_ERROR;
+            }
+            
+            $user->setPassword($password_2);
+            $user->generateAuthKey();
+            
+            if($user->validate())
+            {
+                if($user->update()){
+                    BaseConsole::output("User '{$user->username}' password was updated.".PHP_EOL);
+                }else{
+                    BaseConsole::output("Can't update user '{$user->username}' password.".PHP_EOL);
+                }
+            }else{
+                BaseConsole::output("User '{$user->username}' is invalide.".PHP_EOL);
+                
+                foreach ($user->errors as $error_key=>$error_item){
+                    BaseConsole::stdout($error_key.':'.$error_item.PHP_EOL);
+                }
+            }
+        }catch (\Exception $e){
+            BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
+            return ExitCode::UNSPECIFIED_ERROR;
+        }catch (\Throwable $e){
+            BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
+            return ExitCode::UNSPECIFIED_ERROR;
+        }
+        return ExitCode::OK; 
+    }
+    
+    /**
+     * Set email for user
+     */
+    public function actionSetEmail()
+    {
+        try{
+            $user_cls=static::$user_cls;
+            $app=Yii::$app;
+            
+            $user_name=BaseConsole::prompt('Enter user name:');
+            
+            $user=User::findOne(["username"=>$user_name]);
+            
+            if(Vrbl::isNull($user)){
+                BaseConsole::stdout("There is no such user '{$user_name}'.".PHP_EOL);
+                return ExitCode::UNSPECIFIED_ERROR;
+            }
+            
+            $email = BaseConsole::prompt("Insert email:");
+            $user->email=$email;
+            
+            if($user->validate())
+            {
+                if($user->update()){
+                    BaseConsole::output("User '{$user->username}' email was updated.".PHP_EOL);
+                }else{
+                    BaseConsole::output("Can't update user '{$user->username}' email.".PHP_EOL);
+                }
+            }else{
+                BaseConsole::output("User '{$user->username}' is invalide.".PHP_EOL);
+                
+                foreach ($user->errors as $error_key=>$error_item){
+                    BaseConsole::stdout($error_key.':'.$error_item.PHP_EOL);
+                }
+            }
+        }catch (\Exception $e){
+            BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
+            return ExitCode::UNSPECIFIED_ERROR;
+        }catch (\Throwable $e){
+            BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
+            return ExitCode::UNSPECIFIED_ERROR;
+        }
+        return ExitCode::OK;
+    }
+    
+    /**
+     * Enable user (set status property to User::STATUS_ACTIVE).
+     */
+    public function actionEnable()
+    {
+        try{
+            $user_cls=static::$user_cls;
+            $app=Yii::$app;
+            
+            $user_name=BaseConsole::prompt('Enter user name:');
+            
+            $user=User::findOne(["username"=>$user_name]);
+            
+            if(Vrbl::isNull($user)){
+                BaseConsole::stdout("There is no such user '{$user_name}'.".PHP_EOL);
+                return ExitCode::UNSPECIFIED_ERROR;
+            }
+            
+            
+            $user->status=User::STATUS_ACTIVE;
+            
+            if($user->validate())
+            {
+                if($user->update()){
+                    BaseConsole::output("User '{$user->username}' was enabled.".PHP_EOL);
+                }else{
+                    BaseConsole::output("Can't enable user '{$user->username}'.".PHP_EOL);
+                }
+            }else{
+                BaseConsole::output("User '{$user->username}' is invalide.".PHP_EOL);
+                
+                foreach ($user->errors as $error_key=>$error_item){
+                    BaseConsole::stdout($error_key.':'.$error_item.PHP_EOL);
+                }
+            }
+        }catch (\Exception $e){
+            BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
+            return ExitCode::UNSPECIFIED_ERROR;
+        }catch (\Throwable $e){
+            BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
+            return ExitCode::UNSPECIFIED_ERROR;
+        }
+        return ExitCode::OK;
+    }
+    
+    /**
+     * Disable user (set status property to User::STATUS_DELETED).
+     */
+    public function actionDisable()
+    {
+        try{
+            $user_cls=static::$user_cls;
+            $app=Yii::$app;
+            
+            $user_name=BaseConsole::prompt('Enter user name:');
+            
+            $user=User::findOne(["username"=>$user_name]);
+            
+            if(Vrbl::isNull($user)){
+                BaseConsole::stdout("There is no such user '{$user_name}'.".PHP_EOL);
+                return ExitCode::UNSPECIFIED_ERROR;
+            }
+            
+            
+            $user->status=User::STATUS_DELETED;
+            
+            if($user->validate())
+            {
+                if($user->update()){
+                    BaseConsole::output("User '{$user->username}' was disabled.".PHP_EOL);
+                }else{
+                    BaseConsole::output("Can't disable user '{$user->username}'.".PHP_EOL);
+                }
+            }else{
+                BaseConsole::output("User '{$user->username}' is invalide.".PHP_EOL);
+                
+                foreach ($user->errors as $error_key=>$error_item){
+                    BaseConsole::stdout($error_key.':'.$error_item.PHP_EOL);
+                }
+            }
+        }catch (\Exception $e){
+            BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
+            return ExitCode::UNSPECIFIED_ERROR;
+        }catch (\Throwable $e){
+            BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
+            return ExitCode::UNSPECIFIED_ERROR;
+        }
+        return ExitCode::OK;
+    }
+    
     
     /**
      * Add user to system.
@@ -83,12 +282,12 @@ class UserController extends Controller
             if($user->validate())
             {
                 if($user->insert()){
-                    BaseConsole::output("User $user->username added.".PHP_EOL);
+                    BaseConsole::output("User '{$user->username}' added.".PHP_EOL);
                 }else{
-                    BaseConsole::output("Can't add user $user->username.".PHP_EOL);
+                    BaseConsole::output("Can't add user '{$user->username}'.".PHP_EOL);
                 }
             }else{
-                BaseConsole::output("User is invalide.".PHP_EOL);
+                BaseConsole::output("User '{$user->username}' is invalide.".PHP_EOL);
                 
                 foreach ($user->errors as $error_key=>$error_item){
                     BaseConsole::stdout($error_key.':'.$error_item.PHP_EOL);
@@ -96,12 +295,12 @@ class UserController extends Controller
             }
         }catch (\Exception $e){
             BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
-             return -1;
+            return ExitCode::UNSPECIFIED_ERROR;
         }catch (\Throwable $e){
             BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
-            return -1;
+            return ExitCode::UNSPECIFIED_ERROR;
         }
-        return 0; 
+        return ExitCode::OK; 
     }
     
     /**
@@ -113,7 +312,8 @@ class UserController extends Controller
             $user_name=BaseConsole::input("Insert user name:");
             $model=$user_cls::find()->where(['username'=>$user_name])->one();
             
-            if(BaseConsole::confirm("Are you sure?")){
+            if(BaseConsole::confirm("Are you sure?")
+                &&BaseConsole::confirm("Are you realy sure?")){
                 if(is_null($model)){
                     BaseConsole::output("No such user '$user_name'.".PHP_EOL);
                 }else{
@@ -128,12 +328,12 @@ class UserController extends Controller
             }
         }catch (\Exception $e){
             BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
-             return -1;
+             return ExitCode::UNSPECIFIED_ERROR;
         }catch (\Throwable $e){
             BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
-            return -1;
+            return ExitCode::UNSPECIFIED_ERROR;
         }
-        return 0;
+        return ExitCode::OK;
     }
     
     /**
@@ -143,8 +343,7 @@ class UserController extends Controller
         try{
             $user_cls=static::$user_cls;
             if(BaseConsole::confirm("Are you sure?")
-                &&(!BaseConsole::confirm("Are you crazy?"))){
-                //if(){
+                &&(BaseConsole::confirm("Are you realy sure?"))){
                     try{
                         $user_cls::deleteAll();
                         BaseConsole::output('Users were droped.'.PHP_EOL);
@@ -152,20 +351,17 @@ class UserController extends Controller
                         BaseConsole::output("Can\'t drop all users.".PHP_EOL);
                         throw $e;
                     }
-                //}else{
-                    
-                //}
             }else{
                 BaseConsole::output("You discard this action.");
             }
         }catch (\Exception $e){
             BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
-             return -1;
+            return ExitCode::UNSPECIFIED_ERROR;
         }catch (\Throwable $e){
             BaseConsole::stdout($e->getMessage().PHP_EOL.$e->getTraceAsString());
-            return -1;
+            return ExitCode::UNSPECIFIED_ERROR;
         }
-        return 0;
+        return ExitCode::OK;
     } 
 }
 
