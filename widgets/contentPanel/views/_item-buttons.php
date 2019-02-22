8f0de0958f3b13e05c79fa648516dc56c3dda1ea
Ivan <devskyfly@gmail.com>
2019-02-22 15:59:43 +0300

    Fix views.

--- widgets/contentPanel/views/_item-buttons.php
+++ widgets/contentPanel/views/_item-buttons.php
@@ -5,6 +5,7 @@ use devskyfly\php56\types\Obj;
 use devskyfly\yiiModuleAdminPanel\models\contentPanel\AbstractEntity;
 use devskyfly\yiiModuleAdminPanel\models\contentPanel\AbstractItem;
 use devskyfly\yiiModuleAdminPanel\models\contentPanel\AbstractSection;
+use devskyfly\yiiModuleAdminPanel\models\contentPanel\AbstractUnnamedEntity;
 
 /* @var $this \yii\web\View */
 /* @var $item devskyfly\yiiModuleAdminPanel\models\AbstractItem */
@@ -18,23 +19,23 @@ $classes=[
 $delete_action='';
 $item_id_name='';
 
-if(Obj::isA($item, AbstractEntity::class)){
+if(Obj::isA($item, AbstractUnnamedEntity::class)){
     $delete_action='entity-delete';
     $item_id_name='entity_id';
 }elseif(Obj::isA($item, AbstractSection::class)){
     $delete_action='section-delete';
     $item_id_name='section_id';
 }else{
-    throw new \InvalidArgumentException('Param $item is not '.AbstractEntity::class.' or '.AbstractSection::class.'type.');
+    throw new \InvalidArgumentException('Param $item is not '.AbstractUnnamedEntity::class.' or '.AbstractSection::class.' type.');
 }
 
 $action='';
-if(Obj::isA($item, AbstractEntity::class)){
+if(Obj::isA($item, AbstractUnnamedEntity::class)){
     $action='section-delete';
 }elseif (Obj::isA($item, AbstractSection::class)){
     $action='entity-delete';
 }else{
-    throw \InvalidArgumentException('Param $item is not '.AbstractItem::class.' type.');
+    throw \InvalidArgumentException('Param $item is not '.AbstractUnnamedEntity::class.' type.');
 }
 ?>
 <?if($item->isNewRecord):?>
